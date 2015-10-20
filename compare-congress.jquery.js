8eca889972c936b87f6f88ec635cc38eba13bc40
Albert <jalbertbowden@gmail.com>
2015-10-20 15:52:04 -0400

    compare congress repo init; first commit -> ping congress api, get .json list of politicians. sort by last name alphabetically, and insert into two select elements for the user to choose from.

---
+++ compare-congress.jquery.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("select[name='person2']").hide();
+
+
+
+
+
+    $.getJSON("https://congress.api.sunlightfoundation.com/legislators?per_page=all&apikey=ebbe9f8409134303aab01479e8f1a42f", function (data) {
+
+        // sort alphabetically by last name
+        var jsonData = data.results;
+        jsonData.sort(function(a,b) {
+            if (a['last_name'] < b['last_name']) return -1;
+            if (a['last_name'] > b['last_name']) return 1;
+            return 0;
+        });
+
+        $.each(jsonData, function () {
+            var ele = $("<option>").attr('value', this['bioguide_id'])
+            					   .text(this['first_name'] + " " + this['last_name']);
+           	$("select").append(ele.clone());
+            
+            
+            // show 2nd list of options only after user has selected first. doing this with button and not on selection because user could easily make 
+            // an erroneous choice
+            // another option here is to grey it out until the button is clicked....
+            $(".show").click(function() {
+                $("select[name='person2']").show();
+            });
+
+            
+        });
+    });
+});
