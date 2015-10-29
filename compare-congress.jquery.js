a8347ecbc3a49a1e643e3a8caea52ca27935ca33
Albert <jalbertbowden@gmail.com>
2015-10-29 09:26:11 -0400

    Added vars for urls of api calls, replaced existing api call urls with vars

--- compare-congress.jquery.js
+++ compare-congress.jquery.js
@@ -1,11 +1,48 @@
 $(document).ready(function () {
     $("select[name='person2']").hide();
 
+/*
+congress.api.sunlightfoundation.com/amendments?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/bills?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/bills/search?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/committees?member_ids=A000372&apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/districts/locate?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/floor_updates?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/hearings?dc=false&apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/hearings?dc=true&apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/hearings?dc=null&apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/legislators/locate?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/legislators?apikey=ebbe9f8409134303aab01479e8f1a42f
+
+congress.api.sunlightfoundation.com/nominations?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/votes?apikey=ebbe9f8409134303aab01479e8f1a42f
+congress.api.sunlightfoundation.com/upcoming_bills?apikey=ebbe9f8409134303aab01479e8f1a42f
+*/
+var apiURLStub1 = 'https://congress.api.sunlightfoundation.com/';
+
+var apiURLStubAPIKey = '&apikey=ebbe9f8409134303aab01479e8f1a42f';
 
 
 
+var apiURLStubAmendments = "amendments?";
+var apiURLStubBills = "bills?";
+var apiURLStubBillsSearch = "bills/search?";
+var apiURLStubCommittees = 'committees?';
+var apiURLStubDistrictsLocate = "districts/locate?";
+var apiURLStubFloorUpdates = "floor_updates?";
+var apiURLStubHearingsDCFalse = "hearings?dc=false?";
+var apiURLStubHearingsDCTrue = "hearings?dc=true?";
+var apiURLStubHearingsDCNull = "hearings?dc=null?";
+var apiURLStubLegislators = "legislators?";
+var apiURLStubLegislatorsLocate = "legislators/locate?";
+var apiURLStubNominations = "nominations?";
+var apiURLStubUpcomingBills = "upcoming_bills?";
+var apiURLStubVotes = "votes?";
+var apiURLStubAllPerPage = "per_page=all";
 
-    $.getJSON("https://congress.api.sunlightfoundation.com/legislators?per_page=all&apikey=ebbe9f8409134303aab01479e8f1a42f", function (data) {
+var initialAPICall = apiURLStub1 + apiURLStubLegislators + apiURLStubAllPerPage + apiURLStubAPIKey;
+
+    $.getJSON(initialAPICall, function (data) {
 
         // sort alphabetically by last name
         var jsonData = data.results;
@@ -55,9 +92,6 @@ var personSelectEvent = function() {
 $('.person-selector').change(personSelectEvent);
 
 
-var apiURLStub1 = 'https://congress.api.sunlightfoundation.com/';
-var apiURLStubCommittees = 'committees?';
-var apiURLStubAPIKey = '&apikey=ebbe9f8409134303aab01479e8f1a42f';
 
 function displayVal1() {
   var selectedVal1 = $("#person1").val();
@@ -65,7 +99,19 @@ function displayVal1() {
   var apiURLStubMemberID1 = 'member_ids=' + selectedVal1;
   var apiURLCommittee1 = apiURLStub1 + apiURLStubCommittees + apiURLStubMemberID1 + apiURLStubAPIKey;
   console.log(apiURLCommittee1);
+
+    $.getJSON(apiURLCommittee1, function (data) {
+
+    });
+
+
+
+
 }
+
+
+
+// don't need this at some point
 $("#person1").change(displayVal1);
 displayVal1();
 
@@ -78,6 +124,8 @@ function displayVal2() {
   var apiURLCommittee2 = apiURLStub1 + apiURLStubCommittees + apiURLStubMemberID2 + apiURLStubAPIKey;
   console.log(apiURLCommittee2);
 }
+
+// don't need this at some point
 $("#person2").change(displayVal2);
 displayVal2();
 
